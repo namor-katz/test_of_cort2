acc16e9d4187893f7607a58fcb82f840a49ab432
RomanK <namor925@gmail.com>
2020-10-22 20:28:29 +0700

    delete method

--- src/main/java/com/example/demo/controllers/rest/LanguageRestController.java
+++ src/main/java/com/example/demo/controllers/rest/LanguageRestController.java
@@ -2,20 +2,26 @@ package com.example.demo.controllers.rest;
 
 import com.example.demo.models.Language;
 import com.example.demo.service.interfaces.LanguageService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(value = {"/language", "/language/{name}"})
+@RequestMapping()
 public class LanguageRestController {
 
     private final LanguageService languageService;
 
-    @GetMapping()
+    @GetMapping(value = {"language", "/language/{name}"})
     public List<Language> getLanguage(@PathVariable(required = false) String name) {
         if (name ==  null) {
             return languageService.getAllLanguage();
@@ -32,4 +38,24 @@ public class LanguageRestController {
         System.out.println("получен запрос post!");
     }
 
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "ok")})
+    @DeleteMapping("/language/{name}")
+    public Map<String, String> deleteLanguage(@PathVariable(name = "name") String name) {
+        HashMap<String, String> map = new HashMap<>();
+        if(name == null) {
+            System.out.println("сорян, чо удалять то?");
+//            HashMap<String, String> map = new HashMap<>();
+            map.put("", "");
+        }
+        else {
+            Language l = languageService.findLanguageByName(name);
+            Long id = l.getId();
+            languageService.deleteLanguage(id);
+//            return new ResponseEntity(HttpStatus.OK);
+//            System.out.println("удалено успешно!");
+//            HashMap<String, String> map = new HashMap<>();
+            map.put("state", "ok");
+        }
+        return map;
+    }
 }
